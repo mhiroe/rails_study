54abde741bab742a183fa4c1ecadaa31adc96ec2
mhiroe <hmregist@gmail.com>
2016-11-08 12:38:31 +0900

    new files

---
+++ dotinstall/00_introduction.rb
@@ -0,0 +1,16 @@
+# ruby on rails 4
+
+# 00 introduction
+
+# 知識
+# HTML/CSS/JavaScript/Jquery
+# sqlite/activerecord
+
+
+# 環境
+# ruby 2.0
+# rails 4.0
+
+# ドメイン dev.dotinstall.com:3000
+# → hostに記述しておくこと
+
